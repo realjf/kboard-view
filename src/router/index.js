e80aed0488187a95328709d9fdbaa007bffd3224
chenjiefeng <chenjiefeng@4399inc.com>
2018-12-17 19:40:12 +0800

    components update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Log from '@/components/logs/Log'
+import Login from "@/components/Login";
 
 Vue.use(Router);
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/log',
       name: 'Log',
       component: Log
+      },{
+       path: '/login',
+       name: 'Login',
+       component: Login
       },{
       path: '*',
       redirect: '/login',
