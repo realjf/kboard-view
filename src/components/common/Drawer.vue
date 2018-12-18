a03a969953ccb5212c1db91fd03827cc6ac21547
chenjiefeng <chenjiefeng@4399inc.com>
2018-12-18 19:41:27 +0800

    components update

---
+++ src/components/common/Drawer.vue
@@ -0,0 +1,22 @@
+<template>
+    <Button @click="value2 = true" type="primary">Open</Button>
+    <Drawer title="Basic Drawer" placement="left" :closable="false" v-model="value2">
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+    </Drawer>
+</template>
+<script>
+    export default {
+        name: "Drawer",
+        data () {
+            return {
+                value2: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
